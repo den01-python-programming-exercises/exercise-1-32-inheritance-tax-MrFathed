ec34a8bd335de47258782f6fab2b764483ec8243
Justin Turner <jmturner638@gmail.com>
2021-07-27 22:35:50 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,26 @@
 def main():
     #write your code below this line
+    inheritance =  int(input("Inheritance:"))
+    years = int(input("Years since death:"))
+    taxable = inheritance - 325000
+    tax = 0.0
+
+    if years < 3:
+        tax_rate = 0.40
+    elif years < 4:
+        tax_rate = 0.32
+    elif years < 5:
+        tax_rate = 0.24
+    elif years < 6:
+        tax_rate = 0.16
+    elif years < 7:
+        tax_rate = 0.08
+    else:
+        tax_rate = 0.0
+
+    tax = taxable * tax_rate
+
+    print("Tax: " + str(tax))
 
 if __name__ == '__main__':
     main()
